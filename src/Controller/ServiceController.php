fe430db7f6830d07299faeb1687cab99c33ba558
Prudence ASSOGBA <jprud67@gmail.com>
2025-09-20 08:18:31 +0800

    Sync with matrix message 314461739315502

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 
 class ServiceController extends AbstractController
 {
@@ -49,7 +50,7 @@ class ServiceController extends AbstractController
 
     #[Route('/service/{slug}', name: 'service_show')]
     public function show(
-        Service $service,
+        #[MapEntity(mapping: ['slug' => 'slug'])] Service $service,
         Request $request,
         LanguageRepository $languageRepository
     ): Response {
